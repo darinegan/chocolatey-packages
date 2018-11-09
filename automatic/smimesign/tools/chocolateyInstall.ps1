40b1fb62f81c638e6172cc5f6b98c3a4a8b3055c
Darin Egan <darinegan@gmail.com>
2018-11-09 11:38:11 +0000

    Bump smimesign

--- automatic/smimesign/tools/chocolateyInstall.ps1
+++ automatic/smimesign/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@ $installDir  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = 'smimesign'
-  url            = 'https://github.com/github/smimesign/releases/download/0.0.6/smimesign-windows-386-0.0.6.zip'
-  url64Bit       = 'https://github.com/github/smimesign/releases/download/0.0.6/smimesign-windows-amd64-0.0.6.zip'
-  checksum       = '9A13D00AA02C0A5D277C030297D09F10A467F31E6740F1520A08E09A23046323'
-  checksum64     = '2A2F946E31F2D74EADCDCD97B7BFC69298CEE2F11CF7CB03C604D28FA1B34CD3'
+  url            = 'https://github.com/github/smimesign/releases/download/0.0.7/smimesign-windows-386-0.0.7.zip'
+  url64Bit       = 'https://github.com/github/smimesign/releases/download/0.0.7/smimesign-windows-amd64-0.0.7.zip'
+  checksum       = '6a974a54763c8eef7085cf53b1fb49da90c7e9a2b9c0fd7360aee73042258681'
+  checksum64     = 'e8e59a7a13635307e42225c2103b3d48ca004586d9d0382e225ea1e0594c8ff5'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $installDir
