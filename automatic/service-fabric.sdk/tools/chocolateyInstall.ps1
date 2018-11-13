10cb48af5c780f8ab5d50a9cc2162b2a8fa4d4d9
Darin Egan <darinegan@users.noreply.github.com>
2018-11-13 21:58:41 +0000

    Add Service Fabric (#1)

---
+++ automatic/service-fabric.sdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$logPath = Join-Path $env:TEMP 'chocolatey' | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
+$logFile = Join-Path $logPath 'install.log'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = $env:ChocolateyPackageTitle
+  fileType       = 'msi'
+  url            = 'https://download.microsoft.com/download/8/9/A/89AC6EFC-7885-4D3E-A3B3-30C682BC702C/MicrosoftServiceFabricSDK.2.6.220.msi'
+  checksum       = '8c2624f46719623b897e86032bd3aa887d6b2414ad22493547d607197b3362c4'
+  checksumType   = 'sha256'
+  silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
