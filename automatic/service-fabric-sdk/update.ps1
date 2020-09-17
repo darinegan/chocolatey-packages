aa6e59f82952429b7375f5dfd1c799639a2f74a7
Jakub Bereżański <kuba@berezanscy.pl>
2020-09-17 21:44:29 +0200

    service-fabric-sdk: Automatically update service-fabric dependency version

--- automatic/service-fabric-sdk/update.ps1
+++ automatic/service-fabric-sdk/update.ps1
@@ -7,12 +7,16 @@ function global:au_SearchReplace {
             "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
             "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
         }
+        'service-fabric-sdk.nuspec' = @{
+            "(dependency\s+id=`"service-fabric`"\s+version=`")([^`"]+)" = "`${1}$($Latest.RuntimeVersion)"
+        }
      }
 }
 
 function global:au_GetLatest {
     $Latest = @{
         Version = '4.1.456'
+        RuntimeVersion = '7.1.456'
         URL32 = 'https://download.microsoft.com/download/9/3/0/930f9307-52ef-4597-9ae9-f534415e77eb/MicrosoftServiceFabricSDK.4.1.456.msi'
         ChecksumType32 = 'sha256'
     }
