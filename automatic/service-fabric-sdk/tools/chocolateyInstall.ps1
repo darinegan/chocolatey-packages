3cbc0afb8441eb07d2e3c545dc382c34a0e689d5
Jakub Bereżański <kuba@berezanscy.pl>
2020-09-27 00:49:02 +0200

    Update SF to 7.1 CU5
    https://github.com/microsoft/service-fabric/blob/9d547b43249c93730221f0c38b230829e9902956/release_notes/Service-Fabric-71CU5-releasenotes.md
    
    Note: the document is named "71CU5", but the SF update is described as
    "Fourth Refresh". According to [1], this is intentional (CU4 was
    skipped).
    
    [1] https://github.com/microsoft/service-fabric/issues/1033#issuecomment-694580695

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/9/3/0/930f9307-52ef-4597-9ae9-f534415e77eb/MicrosoftServiceFabricSDK.4.1.456.msi'
-  checksum       = '1deab0791e0d925ce302f0b66f4e5960030c6e8925dc3b58559f0fcac19e5fc8'
+  url            = 'https://download.microsoft.com/download/e/3/c/e3ccf2e1-2c80-48b3-9a8d-ce0dbd67bb77/MicrosoftServiceFabricSDK.4.1.458.msi'
+  checksum       = '4bca4a80ecec46d445896e7a31145cc399f6ca40c0237ab5554c31ade5a4a462'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
