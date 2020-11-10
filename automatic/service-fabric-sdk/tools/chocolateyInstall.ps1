ba1fc33430ea280f4dfee20ef8b79e7f1cd49c7a
Jakub Bereżański <kuba@berezanscy.pl>
2020-11-10 12:12:30 +0100

    Update SF to 7.2
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service-Fabric-72-releasenotes.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/e/3/c/e3ccf2e1-2c80-48b3-9a8d-ce0dbd67bb77/MicrosoftServiceFabricSDK.4.1.458.msi'
-  checksum       = '4bca4a80ecec46d445896e7a31145cc399f6ca40c0237ab5554c31ade5a4a462'
+  url            = 'https://download.microsoft.com/download/5/9/d/59d472e7-4c84-4cc5-8622-aeed97895192/MicrosoftServiceFabricSDK.4.2.413.msi'
+  checksum       = '562b3b4e67740975761071490b9f80dcb586484e960e25fe5a5eb5008ca5c674'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
