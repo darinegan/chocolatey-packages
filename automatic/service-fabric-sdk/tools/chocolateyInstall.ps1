5a282de225577c88bcf9b578a796f31ff20e7e1e
Jakub Bereżański <kuba@berezanscy.pl>
2022-02-21 18:15:14 +0100

    Update SF to 8.2 CU2 (#15)
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service_Fabric_ReleaseNotes_82CU2.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.5.2.1363.msi'
-  checksum       = '198f0eec125705ce97d8738a7e89e1f23b3ccd194f40ab4629a909d99150ea9d'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.5.2.1486.msi'
+  checksum       = 'c6e065d5a7f118b08bb3de9395a1268afb116a44139ebaf53ec152642a37218b'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
