eff454d73a88c2b3b7569c077dc280d8ba3be0dc
Kassoul <5912810+Kassoul@users.noreply.github.com>
2022-09-19 18:34:26 +0200

    Update SF to 9.0CU3

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.0.1048.msi'
-  checksum       = '52a15c10803bb1cd1155005caca6d210e641c9e56fd19371ddb5eedea74873d8'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.0.1107.msi'
+  checksum       = 'c002dcd0950f346943c9e501c849cd5eae175a6834f63ed6d86c42b930aa433e'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
