7cebd5aab337a4a823ec0209742c6a2600430207
Michael Sabin <michael.sabin@citrix.com>
2022-11-09 16:15:16 -0500

    Update to service fabric 9.1.1390
    Revert smime package change

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.0.1107.msi'
-  checksum       = 'c002dcd0950f346943c9e501c849cd5eae175a6834f63ed6d86c42b930aa433e'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.1.1390.msi'
+  checksum       = '318e1ffcbe7e038c0c831803c7a73d54e668ee269d893ef36c0931b3552c4bc6'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
