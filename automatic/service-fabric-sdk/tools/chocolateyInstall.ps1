7aabda7674572622b5c69fa775236605688e37a7
Jakub Bereżański <kuba@berezanscy.pl>
2023-09-22 11:36:31 +0200

    Update SF to 9.1 CU5 (#21)
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service_Fabric_ReleaseNotes_91CU5.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.1.1583.msi'
-  checksum       = '3f9548a1bcca80a8a024a7740098c1dccc84634a0f6a7595b318971372eeacdd'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.6.1.1833.msi'
+  checksum       = '883a6ae52a7b61fd6a567819c142a3d009b00c16698d1bc742e15184803d939b'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
