00f92801f5c3509d35970bc63bcbcfb8e08c0c52
William <williamoconnor@me.com>
2023-11-10 12:55:12 +0000

    Update to version SF 10 1949 (#23)

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.7.0.1816.msi'
-  checksum       = '00f138b5e9d1e12a590f5f743ccd292c4772290d0f28044206f8270dcddeb421'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.7.0.1949.msi'
+  checksum       = 'B622775EB6D9030BA78617E2C1C6D095794E63A5600CD32A8791678163948F3E'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
