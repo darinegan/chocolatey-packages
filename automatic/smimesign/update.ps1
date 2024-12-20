50f6ce0f3e1a4408462180ff02edbd60a7f89d95
Darin Egan <darinegan@users.noreply.github.com>
2024-12-20 11:30:11 +0000

    Bump smimesign to latest

--- automatic/smimesign/update.ps1
+++ automatic/smimesign/update.ps1
@@ -3,19 +3,19 @@ Import-Module au
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(?i)^(\s*url\s*=\s*)'.*'" = "`${1}'$($Latest.URL32)'"
-            "(?i)^(\s*url64Bit\s*=\s*)'.*'" = "`${1}'$($Latest.URL64)'"
-            "(?i)^(\s*checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
-            "(?i)^(\s*checksum64\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum64)'"
-            "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
+            "(?i)^(\s*url\s*=\s*)'.*'"            = "`${1}'$($Latest.URL32)'"
+            "(?i)^(\s*url64Bit\s*=\s*)'.*'"       = "`${1}'$($Latest.URL64)'"
+            "(?i)^(\s*checksum\s*=\s*)'.*'"       = "`${1}'$($Latest.Checksum32)'"
+            "(?i)^(\s*checksum64\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum64)'"
+            "(?i)^(\s*checksumType\s*=\s*)'.*'"   = "`${1}'$($Latest.ChecksumType32)'"
             "(?i)^(\s*checksumType64\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType64)'"
         }
-     }
+    }
 }
 
 function global:au_GetLatest {
     $request = [System.Net.WebRequest]::Create('https://github.com/github/smimesign/releases/latest')
-    $request.AllowAutoRedirect=$true
+    $request.AllowAutoRedirect = $true
     $response = $request.GetResponse()
     $responseUri = $response.ResponseUri
 
@@ -23,9 +23,9 @@ function global:au_GetLatest {
     $version = $tag -replace '^v?'
 
     $Latest = @{
-        Version = $version
-        URL32 = 'https://github.com/github/smimesign/releases/download/{0}/smimesign-windows-386-{1}.zip' -f $tag, $version
-        URL64 = 'https://github.com/github/smimesign/releases/download/{0}/smimesign-windows-amd64-{1}.zip' -f $tag, $version
+        Version        = $version
+        URL32          = 'https://github.com/github/smimesign/releases/download/{0}/smimesign-windows-386-{0}.zip' -f $tag
+        URL64          = 'https://github.com/github/smimesign/releases/download/{0}/smimesign-windows-amd64-{0}.zip' -f $tag
         ChecksumType32 = 'sha256'
         ChecksumType64 = 'sha256'
     }
