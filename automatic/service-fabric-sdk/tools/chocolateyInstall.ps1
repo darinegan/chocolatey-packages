d94e7b694a636641ff3abacbd4513ad977d9a2e5
Shane Kim <shane.kim@betfanatics.com>
2026-04-15 10:26:42 -0700

    update service fabric runtime (11.4.205.1) and sdk (8.4.205)

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.2.274.msi'
-  checksum       = 'c0aed1f6ef848be5e40826ef428b962c5260c74ec1daa84ad73dc8365ecea1e6'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.4.205.msi'
+  checksum       = '74632d7bf4c11b91d365a5d5fbc5c1ac5ad408a99874edc96d04f0c860d15710'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
