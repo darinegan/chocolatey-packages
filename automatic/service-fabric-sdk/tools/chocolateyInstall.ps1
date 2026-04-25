acb13a8d8c079f17d9bc64a07c19e9d7c15e0071
Jakub Bereżański <kuba@berezanscy.pl>
2026-04-25 15:42:29 +0200

    Update SF to 11.4 Hotfix 1 (#32)
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service_Fabric_ReleaseNotes_114_1-Hotfix.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.2.274.msi'
-  checksum       = 'c0aed1f6ef848be5e40826ef428b962c5260c74ec1daa84ad73dc8365ecea1e6'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.4.268.msi'
+  checksum       = 'd92ecdbd0b5777cf0410d5fe873cd5325a87976872a93d6e79a1695170602401'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
