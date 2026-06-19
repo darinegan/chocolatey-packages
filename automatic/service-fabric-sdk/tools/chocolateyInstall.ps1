dcc21606b6787eafcffe4046649a2e13da5acc90
Jakub Bereżański <kuba@berezanscy.pl>
2026-06-19 18:57:38 +0200

    Update SF to 11.5 Hotfix 1
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service_Fabric_ReleaseNotes_115_1-Hotfix.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.4.268.msi'
-  checksum       = 'd92ecdbd0b5777cf0410d5fe873cd5325a87976872a93d6e79a1695170602401'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.5.116.msi'
+  checksum       = 'cc5317a78fba060df0034b8ff1ff203bafbba3a190623280bd14223baa8c7591'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
