4a6aff79c2c257b8a69196f1ba56897dde1e7fd1
Jakub Bereżański <kuba@berezanscy.pl>
2026-07-27 17:28:06 +0200

    Update SF to 11.6.235 (#46)
    https://github.com/microsoft/service-fabric/blob/master/release_notes/Service_Fabric_ReleaseNotes_116.md

--- automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
+++ automatic/service-fabric-sdk/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = $env:ChocolateyPackageTitle
   fileType       = 'msi'
-  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.5.116.msi'
-  checksum       = 'cc5317a78fba060df0034b8ff1ff203bafbba3a190623280bd14223baa8c7591'
+  url            = 'https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.6.235.msi'
+  checksum       = '85cd6d8288ebec6762520ec636bbb1aeee5cf51710d3f9a1ccdb4c93f8440b2e'
   checksumType   = 'sha256'
   silentArgs     = '/quiet /qn /norestart /l*v "{0}" IACCEPTEULA=yes' -f $logFile
   validExitCodes = @(0)
